8950b78ea808138e93b81774ba280070f9724bb4
CarsdanDvorachek <cdvchek@gmail.com>
2021-12-07 02:19:56 -0500

    expanded nav buttons

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,8 +3,8 @@ function Header({page,setPage}) {
     return (
         <div className="Header container">
             <div className="row">
-                <h1 className="col-8 my-2">Carsdan Dvorachek</h1>
-                <div className="row col-4 text-center">
+                <h1 className="col-7 my-2">Carsdan Dvorachek</h1>
+                <div className="row col-5 text-center">
                     <h5 className="col-2 my-4" onClick={()=>setPage('home')}>Home</h5>
                     <h5 className="col-2 my-4" onClick={()=>setPage('work')}>Work</h5>
                     <h5 className="col-2 my-4" onClick={()=>setPage('contact')}>Contact</h5>
