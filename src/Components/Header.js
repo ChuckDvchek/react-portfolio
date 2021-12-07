c72fa2d215c1cc4b37e7eb41dadb93d14f8d7413
CarsdanDvorachek <cdvchek@gmail.com>
2021-12-07 02:16:49 -0500

    fixed navbar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,11 +5,11 @@ function Header({page,setPage}) {
             <div className="row">
                 <h1 className="col-8 my-2">Carsdan Dvorachek</h1>
                 <div className="row col-4 text-center">
-                    <h5 className="col-3 my-4" onClick={()=>setPage('home')}>Home</h5>
-                    <h5 className="col-3 my-4" onClick={()=>setPage('work')}>Work</h5>
-                    <h5 className="col-3 my-4" onClick={()=>setPage('contact')}>Contact</h5>
-                    <h5 className="col-3 my-4" onClick={()=>setPage('resume')}>Resume</h5>
-                    <h5 className="col-3 my-4" onClick={()=>setPage('about')}>About</h5>
+                    <h5 className="col-2 my-4" onClick={()=>setPage('home')}>Home</h5>
+                    <h5 className="col-2 my-4" onClick={()=>setPage('work')}>Work</h5>
+                    <h5 className="col-2 my-4" onClick={()=>setPage('contact')}>Contact</h5>
+                    <h5 className="col-2 my-4" onClick={()=>setPage('resume')}>Resume</h5>
+                    <h5 className="col-2 my-4" onClick={()=>setPage('about')}>About</h5>
                 </div>
             </div>
         </div>
