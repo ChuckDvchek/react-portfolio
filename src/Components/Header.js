73a7debb7eb0cd2d88137909feeb48b8a4c2a9a9
CarsdanDvorachek <cdvchek@gmail.com>
2021-12-07 02:14:03 -0500

    changed navbar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,11 +5,11 @@ function Header({page,setPage}) {
             <div className="row">
                 <h1 className="col-8 my-2">Carsdan Dvorachek</h1>
                 <div className="row col-4 text-center">
-                    {(page !== 'home') ? <h5 className="col-3 my-4" onClick={()=>setPage('home')}>Home</h5> : null}
-                    {(page !== 'work') ? <h5 className="col-3 my-4" onClick={()=>setPage('work')}>Work</h5> : null}
-                    {(page !== 'contact') ? <h5 className="col-3 my-4" onClick={()=>setPage('contact')}>Contact</h5> : null}
-                    {(page !== 'resume') ? <h5 className="col-3 my-4" onClick={()=>setPage('resume')}>Resume</h5> : null}
-                    {(page !== 'about') ? <h5 className="col-3 my-4" onClick={()=>setPage('about')}>About</h5> : null}
+                    <h5 className="col-3 my-4" onClick={()=>setPage('home')}>Home</h5>
+                    <h5 className="col-3 my-4" onClick={()=>setPage('work')}>Work</h5>
+                    <h5 className="col-3 my-4" onClick={()=>setPage('contact')}>Contact</h5>
+                    <h5 className="col-3 my-4" onClick={()=>setPage('resume')}>Resume</h5>
+                    <h5 className="col-3 my-4" onClick={()=>setPage('about')}>About</h5>
                 </div>
             </div>
         </div>
